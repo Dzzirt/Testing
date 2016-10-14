50604b347ef18f3936b60811c28903edf70d9f20
Nikita Kuzin <nikitakyza@gmail.com>
2016-10-14 09:20:20 +0300

    lab2 added url validation

---
+++ lab7/src/InvalidUrlException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Nikita on 14.10.2016.
+ */
+public class InvalidUrlException extends Exception {
+}
---
+++ lab7/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Nikita on 14.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws InvalidUrlException {
+        new DeadLinksChecker(args[0]);
+    }
+}
